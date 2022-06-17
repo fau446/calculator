f1fda26d93fe861afac2084d99348c91a5b0956d
Franklin <Franklinau2@gmail.com>
2022-06-17 11:30:34 -0700

    Add button values are displayed

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ function operate(a, b, operator) {
         divide(a, b);
     }
 }
+
+const display = document.querySelector('#display');
+let displayValue = '';
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', function(e) {
+    display.textContent += e.target.innerText;
+    displayValue = displayValue.concat(e.target.innerText);
+    console.log(displayValue);
+})
+)
